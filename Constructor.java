fce1d73fd666a1813d6f93866ab424ebc24cf8a5
liyajose119 <liyajose1515@gmail.com>
2024-10-10 21:57:02 +0530

    Update Constructor.java

--- Constructor.java
+++ Constructor.java
@@ -1,20 +1,23 @@
-class Student {
-int roll;
-String name;
-public Student(String name,int roll) {
-Student1(String Liya,int 1) {
-this.name=name;
-this.roll=roll;
-this.display();
-}
-public Student(String name,int roll) {
-Student 2(String Riya,int 2)
-this.name=name;
-this.roll=roll;
-this.display();
-}
-public static void main(String[] args) {
-System.out.println(Name: " + name);
-System.out.println(Roll: " + roll);
-}
+class Constructor {
+    int roll;
+    String name;
+
+    // Constructor
+    public Constructor(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+        this.display();
+    }
+
+    // Method to display student information
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll: " + roll);
+    }
+
+    public static void main(String[] args) {
+        // Create a new Constructor object
+        Constructor student1 = new Constructor("Liya", 1);
+        Constructor student2 = new Constructor("Riya", 2);
+    }
 }
